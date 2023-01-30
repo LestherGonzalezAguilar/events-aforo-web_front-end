f7fe07ad4880fe3bd0b2ad62db31da3769969e0a
Lésther Josué González Aguilar <lestherjosue@gmail.com>
2023-01-30 10:06:26 -0600

    Revert "operacion merge"
    This reverts commit 2b241e625dcb45179e1cd4ec85a71c7a8bab8f42, reversing
    changes made to 70d8f44e09bfe721d3e933e6a968f622f8b3b92d.

---
+++ components/admin/dashboardAdmin/FilterEvents.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { Button, Center, Flex, Grid, Hide, Show } from "@chakra-ui/react";
+
+import FilterIcon from "../../../assets/filter.svg";
+
+export const FilterEvents = ({ addMensaje }) => {
+  const [filterState, setFilterState] = useState("todos");
+
+  const changeStateFilter = (state) => {
+    setFilterState(state);
+    addMensaje(state);
+  };
+
+  return (
+    <Grid templateColumns="repeat(2, 1fr)">
+      <Flex mt={4}>
+        <Button
+          onClick={() => changeStateFilter("todos")}
+          colorScheme="purple"
+          variant={filterState === "todos" ? "solid" : "outline"}
+          mr={2}
+        >
+          Todos
+        </Button>
+        <Button
+          onClick={() => changeStateFilter("activos")}
+          colorScheme="teal"
+          variant={filterState === "activos" ? "solid" : "outline"}
+          mr={2}
+        >
+          Activos
+        </Button>
+        <Button
+          onClick={() => changeStateFilter("finalizados")}
+          colorScheme="red"
+          variant={filterState === "finalizados" ? "solid" : "outline"}
+          mr={5}
+        >
+          Finalizados
+        </Button>
+        <Hide below="lg">
+          <Flex>
+            <Center>
+              <Image
+                src={FilterIcon}
+                height={20}
+                alt="filter icon"
+                style={{ marginRight: "8px" }}
+              />
+              Filtro
+            </Center>
+          </Flex>
+        </Hide>
+      </Flex>
+    </Grid>
+  );
+};
